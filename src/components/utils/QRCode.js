464d14ddfde022d7d40eeecdc775b81206eeec0f
Prabeen04 <prabeen.strange@gmail.com>
2018-08-18 17:02:39 +0530

    reusable qrcode component created

---
+++ src/components/utils/QRCode.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View, TextInput } from 'react-native'
+import QRCode from 'react-native-qrcode';
+
+class QRCode extends Component {
+    state = {
+        text: 'http://facebook.github.io/react-native/',
+    };
+    render() {
+        return (
+            <View>
+                <QRCode
+                    value={this.props.code}
+                    size={this.props.size || 200}
+                    bgColor={this.props.bgColor || 'purple'}
+                    fgColor={this.props.fgColor || 'white'} />
+            </View>
+        )
+    }
+}
+export default QRCode;
