edaeafb17146c0d2c1565fbc6fb16efcd9a55b2e
Prabeen04 <prabeen.strange@gmail.com>
2018-08-18 11:52:15 +0530

    MainScreen Component created

---
+++ src/Screens/MainScreen.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+class MainScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>MainScreen Component</Text>
+      </View>
+    )
+  }
+}
+export default MainScreen;
+
+const styles = StyleSheet.create({})
