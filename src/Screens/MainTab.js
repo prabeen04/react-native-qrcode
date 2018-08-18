f6a8c2996c190a3547c7889666f1e1f1fa48aeb8
Prabeen04 <prabeen.strange@gmail.com>
2018-08-18 19:55:39 +0530

    MainTab navigation created

---
+++ src/Screens/MainTab.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import { createBottomTabNavigator } from 'react-navigation';
+
+export default class MainTab extends Component {
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    )
+  }
+}
