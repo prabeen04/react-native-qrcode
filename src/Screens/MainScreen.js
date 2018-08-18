7fae8fff442e80056001e0e488c62bf61f60a871
Prabeen04 <prabeen.strange@gmail.com>
2018-08-18 12:09:55 +0530

    qr code implemented

--- src/Screens/MainScreen.js
+++ src/Screens/MainScreen.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, TextInput } from 'react-native'
+import QRCode from 'react-native-qrcode';
 
 class MainScreen extends Component {
-  render() {
-    return (
-      <View>
-        <Text>MainScreen Component</Text>
-      </View>
-    )
-  }
+    state = {
+        text: 'http://facebook.github.io/react-native/',
+    };
+    render() {
+        return (
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={(text) => this.setState({ text: text })}
+                    value={this.state.text}
+                />
+                <QRCode
+                    value={this.state.text}
+                    size={200}
+                    bgColor='purple'
+                    fgColor='white' />
+            </View>
+        )
+    }
 }
 export default MainScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        margin: 10,
+        borderRadius: 5,
+        padding: 5,
+    }
+})
