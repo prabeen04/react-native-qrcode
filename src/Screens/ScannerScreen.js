56a339ab540291838b82f784ca51bf9340fcb4ad
Prabeen04 <prabeen.strange@gmail.com>
2018-08-20 21:06:30 +0530

    QRCode scanner added

--- src/Screens/ScannerScreen.js
+++ src/Screens/ScannerScreen.js
@@ -6,7 +6,19 @@ class ScannerScreen extends Component {
     render() {
         return (
             <View style={styles.container}>
-
+                <QRCodeScanner
+                    onRead={this.onSuccess.bind(this)}
+                    topContent={
+                        <Text style={styles.centerText}>
+                            Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code
+                            </Text> on your computer and scan the QR code. </Text>
+                    }
+                    bottomContent={
+                        <TouchableOpacity style={styles.buttonTouchable}>
+                            <Text style={styles.buttonText}>OK. Got it!</Text>
+                        </TouchableOpacity>
+                    }
+                />
             </View>
         )
     }
